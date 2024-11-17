95fd457d4d11a801562590db94d3796c8c776076
Naxn1a <persecond.sp@gmail.com>
2024-11-18 02:09:02 +0700

    setup nuxt

---
+++ client/nuxt.config.ts
@@ -0,0 +1,31 @@
+export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+  experimental: {
+    scanPageMeta: "after-resolve",
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    normalizeComponentNames: false,
+    defaults: {
+      useAsyncData: {
+        deep: true,
+      },
+    },
+  },
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false,
+    },
+  },
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+  ],
+});
---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./app/**/*.{vue,js,jsx,mjs,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
