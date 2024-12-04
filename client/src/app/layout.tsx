f708315299a7fcca52f4cad88309918806916482
Naxn1a <Persecond.sp@gmail.com>
2024-12-05 02:06:22 +0700

    add font

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Noto_Sans } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Veritas",
   description: "Prototype for Veritas",
 };
+
+const noto = Noto_Sans({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -12,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={noto.className}>{children}</body>
     </html>
   );
 }
