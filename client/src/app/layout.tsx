0c6dace9c39fbd001f38b1f15fffa0e6ff1e88c7
naxn1a <persecond.sp@gmail.com>
2025-02-10 04:14:08 +0700

    structure

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Noto_Sans } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "Veritas",
+  description: "Prototype for Veritas",
+};
+
+const noto = Noto_Sans({
+  weight: ["300"],
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={noto.className}>{children}</body>
+    </html>
+  );
+}
