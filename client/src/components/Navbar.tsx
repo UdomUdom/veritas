e6164440719b85e82ce919d7d279c886a44c2d59
UdomUdom <udomzaark@gmail.com>
2025-02-13 04:59:58 +0700

    login register form

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -60,10 +60,10 @@ export default function Navbar({ NavbarList }: { NavbarList: NavbarProps[] }) {
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+          <Link href="/login">Login</Link>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
+          <Button as={Link} color="primary" href="/register" variant="flat">
             Sign Up
           </Button>
         </NavbarItem>
