f3ff17190ae2293913b57bc0a5384c0268cbc873
naxn1a <persecond.sp@gmail.com>
2025-04-15 04:46:20 +0700

    refactor model

--- server/src/models/blog.ts
+++ server/src/models/blog.ts
@@ -4,9 +4,9 @@ export const BlogModel = t.Object({
   title: t.String(),
   description: t.Optional(t.String()),
   image: t.String(),
-  info: t.Optional(t.String()),
   content: t.Optional(t.String()),
   author: t.String(),
+  category_id: t.Optional(t.String()),
 });
 
 export type BlogType = typeof BlogModel.static;
--- server/src/models/event.ts
+++ server/src/models/event.ts
@@ -2,9 +2,9 @@ import { t } from "elysia";
 
 export const EventModel = t.Object({
   title: t.String(),
-  description: t.String(),
+  description: t.Optional(t.String()),
   image: t.String(),
-  banner: t.String(),
+  banner: t.Optional(t.String()),
   location: t.Optional(t.String()),
   start_date: t.String(),
   end_date: t.Optional(t.String()),
@@ -16,8 +16,8 @@ export const EventModel = t.Object({
       t.Literal("archived"),
     ])
   ),
-  scheduled_publish_at: t.Optional(t.Date()),
   info: t.Optional(t.String()),
+  scheduled_publish_at: t.Optional(t.Date()),
   category_id: t.Optional(t.String()),
   organizer_id: t.Optional(t.String()),
 });
